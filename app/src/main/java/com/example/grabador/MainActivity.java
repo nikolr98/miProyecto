9f36fbad96a6520f82a664c4bca10d89c2409da2
NikolRincon <nikolrincon@wposs.com>
2022-04-29 20:53:16 -0500

    mi primer commit

---
+++ app/src/main/java/com/example/grabador/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.grabador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    private MediaRecorder miGrabacion;
+    private String outputFile = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @SuppressLint("WrongConstant")
+    public void grabar(View view) {
+        outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Grabacion.mp3";
+        miGrabacion = new MediaRecorder();
+        miGrabacion.setAudioSource(MediaRecorder.AudioSource.MIC);
+        miGrabacion.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        miGrabacion.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        miGrabacion.setOutputFile(outputFile);
+        try {
+            miGrabacion.prepare();
+            miGrabacion.start();
+        } catch (IllegalStateException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        Toast.makeText(getApplicationContext(), "La grabación comenzó", Toast.LENGTH_LONG).show();
+    }
+    public void detener(View view) {
+        if (miGrabacion != null) {
+            miGrabacion.stop();
+            miGrabacion.release();
+            miGrabacion = null;
+            Toast.makeText(getApplicationContext(), "El audio  grabado con éxito", Toast.LENGTH_LONG).show();
+        }
+    }
+    public void reproducir(View view) {
+        MediaPlayer m = new MediaPlayer();
+        try {
+            m.setDataSource(outputFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            m.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        m.start();
+        Toast.makeText(getApplicationContext(), "reproducción de audio", Toast.LENGTH_LONG).show();
+    }
+}
